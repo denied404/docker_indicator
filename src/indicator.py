07e2bed2b4a9a320d618766b65f58f4f4fb13030
Anton Lvov <ant.lvov@gmail.com>
2017-11-26 02:37:26 +0500

    Indicator menu being refreshed each second and always shows actual container list / statuses

--- src/indicator.py
+++ src/indicator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from gi.repository import Gtk as gtk
+from gi.repository import Gtk as gtk, GLib as glib
 from gi.repository import AppIndicator3 as appindicator
 import docker
 import os
@@ -26,19 +26,39 @@ class DockerIndicator:
                 appindicator.IndicatorCategory.SYSTEM_SERVICES
         )
         self.indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
-        self.indicator.set_menu(self.build_menu(self.client))
+        self.build_menu()
+        glib.timeout_add(1000, self.refresh_menu)
         signal.signal(signal.SIGINT, signal.SIG_DFL)
         gtk.main()
 
-    def build_menu(self, client):
+    def build_menu(self):
         self.menu = gtk.Menu()
-        for c in client.containers.list(all=True):
+        for c in self.client.containers.list(all=True):
             self.menu.append(self.render_container(c))
         item_quit = gtk.MenuItem('Quit')
         item_quit.connect('activate', quit)
         self.menu.append(item_quit)
         self.menu.show_all()
+        self.indicator.set_menu(self.menu)
         return self.menu
+
+    def refresh_menu(self):
+        containers = self.client.containers.list(all=True)
+        for c in containers:
+            if c.name not in [i.get_label() for i in self.menu
+                              if i.get_label() != 'Quit']:
+                self.menu.append(self.render_container(c))
+        for item in [i for i in self.menu
+                     if i.get_label() != 'Quit']:
+            if item.get_label() not in [c.name for c in containers]:
+                self.menu.remove(item)
+            else:
+                item.set_image(
+                        self.container_status_img(
+                            self.client.containers.get(item.get_label())
+                        )
+                )
+        return True
 
     def container_menu(self, container):
         menu = gtk.Menu()
@@ -53,6 +73,15 @@ class DockerIndicator:
         menu.show_all()
         return menu
 
+    def container_status_img(self, container):
+        if container.status == 'running':
+            icon_path = os.path.join(IMAGE_PATH, 'running.svg')
+        else:
+            icon_path = os.path.join(IMAGE_PATH, 'stopped.svg')
+        img = gtk.Image()
+        img.set_from_file(icon_path)
+        return img
+
     def container_action(self, _, container, action):
         if action == 'start':
             container.start()
@@ -60,15 +89,9 @@ class DockerIndicator:
             container.stop()
 
     def render_container(self, container):
-        if container.status == 'running':
-            icon_path = os.path.join(IMAGE_PATH, 'running.svg')
-        else:
-            icon_path = os.path.join(IMAGE_PATH, 'stopped.svg')
-        img = gtk.Image()
-        img.set_from_file(icon_path)
         menu_entry = gtk.ImageMenuItem(container.name)
         menu_entry.set_always_show_image(True)
-        menu_entry.set_image(img)
+        menu_entry.set_image(self.container_status_img(container))
         menu_entry.set_submenu(self.container_menu(container))
         return menu_entry
 
