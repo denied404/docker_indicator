641603c8c833a4fc9d1509e1874477d1c71eac1d
Anton Lvov <ant.lvov@gmail.com>
2017-11-27 02:14:09 +0500

    When docker service is offline or unavailable, indicator's icon becomes reverted and all containers being excluded from menu

--- src/indicator.py
+++ src/indicator.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
-
+import gi
+gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk as gtk, GLib as glib
+gi.require_version('AppIndicator3', '0.1')
 from gi.repository import AppIndicator3 as appindicator
 import docker
 import os
@@ -9,55 +11,106 @@ import signal
 
 APPINDICATOR_ID = 'docker_indicator'
 IMAGE_DIR = 'img'
-IMAGE_PATH = os.path.join(
-                 os.path.dirname(os.path.realpath(__file__)),
-                 os.pardir,
-                 IMAGE_DIR
-             )
+IMAGE_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                          os.pardir,
+                          IMAGE_DIR)
+DOCKER_ON_IMG = os.path.join(IMAGE_PATH, 'docker-gray.svg')
+DOCKER_OFF_IMG = os.path.join(IMAGE_PATH, 'docker-gray-reverted.svg')
 
 
 class DockerIndicator:
     def __init__(self):
         self.client = docker.DockerClient(
-                base_url='unix://var/run/docker.sock')
+            base_url='unix://var/run/docker.sock')
         self.indicator = appindicator.Indicator.new(
-                APPINDICATOR_ID,
-                os.path.join(IMAGE_PATH, 'docker-gray.svg'),
-                appindicator.IndicatorCategory.SYSTEM_SERVICES
+            APPINDICATOR_ID,
+            '',
+            appindicator.IndicatorCategory.SYSTEM_SERVICES
         )
+        self.refresh_indicator_icon()
         self.indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
         self.build_menu()
-        glib.timeout_add(1000, self.refresh_menu)
+        glib.timeout_add(1000, self.refresh)
         signal.signal(signal.SIGINT, signal.SIG_DFL)
         gtk.main()
 
+    @property
+    def docker_online(self):
+        try:
+            self.client.ping()
+            return True
+        except Exception:
+            return False
+
+    def containers_list(self):
+        if self.docker_online:
+            try:
+                return self.client.containers.list(all=True)
+            except Exception:
+                return []
+        else:
+            return []
+
     def build_menu(self):
         self.menu = gtk.Menu()
-        for c in self.client.containers.list(all=True):
+        for c in self.containers_list():
             self.menu.append(self.render_container(c))
         item_quit = gtk.MenuItem('Quit')
         item_quit.connect('activate', quit)
         self.menu.append(item_quit)
+        self.reorder_menu_items()
         self.menu.show_all()
         self.indicator.set_menu(self.menu)
-        return self.menu
+
+    def reorder_menu_items(self):
+        container_items = [i for i in self.menu
+                           if i.get_label() != 'Quit']
+        quit_item = [i for i in self.menu
+                     if i.get_label() == 'Quit'][0]
+        for k, v in enumerate(
+                sorted(container_items,
+                       key=lambda c: c.get_label())):
+            self.menu.reorder_child(v, k)
+        self.menu.reorder_child(quit_item, len(container_items))
+        self.indicator.set_menu(self.menu)
 
     def refresh_menu(self):
-        containers = self.client.containers.list(all=True)
+        containers = self.containers_list()
+
+        new_containers = False
         for c in containers:
             if c.name not in [i.get_label() for i in self.menu
                               if i.get_label() != 'Quit']:
                 self.menu.append(self.render_container(c))
+                new_containers = True
+
+        # As long as GtkMenu.reorder_child doesn't work on
+        # rendered menu, we need to redraw it each time we
+        # have a new entries to keep them in a right
+        # alphabetical order
+        if new_containers == 1:
+            self.build_menu()
+
         for item in [i for i in self.menu
                      if i.get_label() != 'Quit']:
             if item.get_label() not in [c.name for c in containers]:
                 self.menu.remove(item)
             else:
                 item.set_image(
-                        self.container_status_img(
-                            self.client.containers.get(item.get_label())
-                        )
+                    self.container_status_img(
+                        self.client.containers.get(item.get_label())
+                    )
                 )
+
+    def refresh_indicator_icon(self):
+        if self.docker_online:
+            self.indicator.set_icon(DOCKER_ON_IMG)
+        else:
+            self.indicator.set_icon(DOCKER_OFF_IMG)
+
+    def refresh(self):
+        self.refresh_indicator_icon()
+        self.refresh_menu()
         return True
 
     def container_menu(self, container):
@@ -93,6 +146,7 @@ class DockerIndicator:
         menu_entry.set_always_show_image(True)
         menu_entry.set_image(self.container_status_img(container))
         menu_entry.set_submenu(self.container_menu(container))
+        menu_entry.show()
         return menu_entry
 
     def quit(self, _):
